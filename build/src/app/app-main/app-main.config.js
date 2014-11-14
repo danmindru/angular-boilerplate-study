12375fab1c542c454f5d453e2494cda10290062d
Dan Mindru <mindrudan@gmail.com>
2014-11-14 21:04:16 +0100

    removed build

--- build/src/app/app-main/app-main.config.js
+++
@@ -1,7 +0,0 @@
-ApplicationRootModule.config(rootConfig);
-
-function rootConfig($locationProvider){
-  $locationProvider.hashPrefix('!');
-}
-
-rootConfig.$inject = ['$locationProvider'];
--- build/src/app/app-main/app-main.controller.js
+++
@@ -1,7 +0,0 @@
-ApplicationRootModule.controller('rootController', rootController);
-
-function rootController($log) {
-  $log.warn('App root ctrl is rollin');
-}
-
-rootController.$inject = ['$log'];
