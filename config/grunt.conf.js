5d527956d59d182b10bd5744de25e49c814cae73
Dan Mindru <mindrudan@gmail.com>
2014-11-26 22:56:40 +0100

    added conf files

---
+++ config/grunt.conf.js
@@ -0,0 +1,35 @@
+/*
+ * Configuration variables for Grunt
+ */
+
+module.exports = {
+  ////////////
+  source_dir: './src/',
+  build_dir: './build/',
+  compile_dir: './application/',
+  ////////////
+  build: {
+    ////////////
+    vendor_js: [
+      './vendor/angular/angular.js',
+      './vendor/angular-ui-router/release/angular-ui-router.js',
+      './vendor/angular-mocks/angular-mocks.js',
+      './vendor/stacktrace-js/stacktrace.js',
+      './vendor/loggly-jslogger/src/loggly.tracker.js'
+    ],
+    ////////////
+    vendor_css: [
+      './vendor/bootstrap/dist/css/bootstrap.min.css'
+    ]
+  },
+  ////////////
+  module_file_order: [
+    '**/*.init.js', //EXPLAIN
+    '**/*.config.js', //routes & constants, etc
+    '**/*.run.js',
+    '**/*.service.js', //factories & providers, etc
+    '**/*.controller.js',
+    '**/*.directive.js',
+    '**/*.filter.js'
+  ]
+};
