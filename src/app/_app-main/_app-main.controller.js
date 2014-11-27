eeb291780950ec50032eb6a36b4944caf68c66b1
Dan Mindru <mindrudan@gmail.com>
2014-11-27 22:43:38 +0100

    binded <title> to root module var, root module listens for page title changes (via emit)

--- src/app/_app-main/_app-main.controller.js
+++ src/app/_app-main/_app-main.controller.js
@@ -1,7 +1,34 @@
 angular.module(absConfig.appRootModuleName).controller('RootController', rootController);
 
-function rootController($log) {
-  $log.warn('App root ctrl is rollin');
-}
+rootController.$inject = ['$scope'];
 
-rootController.$inject = ['$log'];
+function rootController($scope) {
+  var vm = this;
+  vm.htmlTitle = 'Angular Boilerplate Study';
+  /*
+   * Event callback on every route change
+   * Scrolls the page as if a 'normal' page would be opened
+   *
+   * Other available params: toParams, fromState, fromParams
+   */
+  $scope.$on('$stateChangeSuccess', function(event, toState){
+    /*
+     * updates the <title> tag
+     * (vm.htmlTitle is binded to the title tag)
+     */
+    if(angular.isDefined(toState.data.pageTitle)){
+      vm.htmlTitle = toState.data.pageTitle + ' - Angular Boilerplate Study';
+    }
+  });
+
+  /*
+   * Listens to controllers that want to emit their titles
+   * Used for custom titles (not from state provider).
+   * With this we can set the titles of 'wildcard' paths
+   * E.g. on 'customers/:Id' -> the page title can be customer.name
+   *
+   */
+  $scope.$on('changedPage', function changedPage(event, pageTitle){
+    vm.htmlTitle = pageTitle;
+  });
+}
