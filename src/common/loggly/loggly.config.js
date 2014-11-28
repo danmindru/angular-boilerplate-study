d35da1c80343984110dff7eb45bf5b1dd40a3b61
Dan Mindru <mindrudan@gmail.com>
2014-11-28 20:27:28 +0100

    patched loggly definition, next time how about thinking before coding

--- src/common/loggly/loggly.config.js
+++ src/common/loggly/loggly.config.js
@@ -1,6 +1,6 @@
 ////// TRACKER DEFINE & KEY IN THIS MODULE TO DECOUPLE
-var _LTracker = _LTracker || [],
-    process.env.LOGGLY_KEY = process.env.LOGGLY_KEY || '';
+var _LTracker = _LTracker || [];
+
 absConfig.pushAfterBootstrap('abs.service.loggly');
 
-angular.module('abs.service.loggly').constant('LOGGLY_KEY', process.env.LOGGLY_KEY);
+angular.module('abs.service.loggly').constant('LOGGLY_KEY', (typeof process !== 'undefined' && process.hasOwnProperty('env.LOGGLY_KEY')) ? process.env.LOGGLY_KEY : '');
