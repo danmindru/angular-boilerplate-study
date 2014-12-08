0e8fecfecb15fcb7d3c4d9abd5b08a06c1392bea
Dan Mindru <mindrudan@gmail.com>
2014-12-08 22:17:30 +0100

    Commented out the grunt files

--- gruntfile.js
+++ gruntfile.js
@@ -28,18 +28,22 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     /*
      * Placed at the top of the compiled source files.
+     * Use the 'banner' -> banner: '<%= meta.banner %>'
+     *
      */
     meta: {
       banner:
         '/*\n' +
-        ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        ' * <%= pkg.repository.url %>\n' +
-        ' *\n' +
-        ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-        ' * Licensed <%= pkg.licenses.type %> <<%= pkg.licenses.url %>>\n' +
+        ' * Compiled <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        ' * Docs @ <%= pkg.repository.url %>\n' +
         ' */\n'
     },
-    //////////////
+    /*
+     * The shell task can run handy shell commands.
+     * To configure the dev/prod server check out the 'sh' files
+     * in './server'.
+     *
+     */
     shell: {
       dev_server: {
         command: './server/build-server.sh'
@@ -54,7 +58,20 @@ module.exports = function(grunt) {
         command: './node_modules/protractor/bin/webdriver-manager update'
       }
     },
-    //////////////
+    /*
+     * The copy task copies files for both build and compile
+     * phases, as well as test files.
+     *
+     * 'build_index' will process the 'index.html' file and write
+     * <script> tags for all files (modules or vendor scripts).
+     *
+     * 'compile_index' will process the 'index.html' file without
+     * providing any scripts. In this case, a single script will
+     * be included: 'app.min.js'.
+     *
+     * See './src/index.html' for more details.
+     *
+     */
     copy: {
       build_app_js: {
         src: [
@@ -165,7 +182,14 @@ module.exports = function(grunt) {
         expand: true
       }
     },
-    /////////////////
+    /*
+     * The clean task is used to remove files / directories that
+     * are used and not needed anymore.
+     *
+     * It's also used to remove the build directory before a new
+     * compile, see 'build_clean' below.
+     *
+     */
     clean: {
       build_clean: ['<%= build_dir %>'],
       build_css_clean: ['<%= build_dir %>/src/assets/css/app.custom.css'],
@@ -178,7 +202,12 @@ module.exports = function(grunt) {
         './src/app//shared/profile-model'
       ]
     },
-    /////////////////
+    /*
+     * The less task compiles all less files.
+     * They will be afterwards concatenated with vendor
+     * stylesheet, see the 'cssmin' task.
+     *
+     */
     less: {
       build_less: {
         src: ['./src/assets/**/*.less'],
@@ -189,7 +218,11 @@ module.exports = function(grunt) {
         }
       }
     },
-    /////////////////
+    /*
+     * The cssmin task minifies css files and merges them into a
+     * single file (done during build).
+     *
+     */
     cssmin: {
       build_css: {
         src: [
@@ -197,10 +230,20 @@ module.exports = function(grunt) {
           './src/assets/**/*.css',
           '<%= build_dir %>/src/assets/css/app.custom.css'
         ],
-        dest: '<%= build_dir %>/src/assets/css/app.min.css'
+        dest: '<%= build_dir %>/src/assets/css/app.min.css',
+        options: {
+          keepSpecialComments: 0,
+          banner: '<%= meta.banner %>'
+        }
       }
     },
-    /////////////////
+    /*
+     * The uglify task mainly minifies and mangles module scripts.
+     * After all scripts are concatenated, it minifies without
+     * mangling to remove comments, line breaks, etc that come
+     * from vendor scripts.
+     *
+     */
     uglify: {
       compile_module_js: {
         options: {
@@ -216,15 +259,20 @@ module.exports = function(grunt) {
         ],
         dest: '<%= compile_dir %>/src/app/modules.min.js'
       },
-      compile_cleanup: {
+      compile_soft_uglify: {
         options: {
-          mangle: false
+          mangle: false,
+          banner: '<%= meta.banner %>'
         },
         src: ['<%= compile_dir %>/src/app/app.min.js'],
         dest: '<%= compile_dir %>/src/app/app.min.js'
       }
     },
-    /////////////////
+    /*
+     * The concat task is used in the compile phase to merge all
+     * scripts into one.
+     *
+     */
     concat: {
       compile_js: {
         src: [
@@ -232,10 +280,18 @@ module.exports = function(grunt) {
           '<%= compile_dir %>/src/app/modules.min.js',
           '<%= compile_dir %>/shared-views/views.min.js'
         ],
-        dest: '<%= compile_dir %>/src/app/app.min.js'
+        dest: '<%= compile_dir %>/src/app/app.min.js',
+        options: {
+          banner: '<%= meta.banner %>'
+        }
       }
     },
-    /////////////////
+    /*
+     * The html2js task is used to convert all views into a module
+     * that adds them to $templateCache. It also minifies HTML
+     * markup.
+     *
+     */
     html2js: {
       options: {
         htmlmin: {
@@ -259,7 +315,11 @@ module.exports = function(grunt) {
         }
       }
     },
-    /////////////////
+    /*
+     * The jshint task detects errors and potential problems in
+     * the JavaScript source and grunt files.
+     *
+     */
     jshint: {
       options: {
         jshintrc: true
@@ -267,7 +327,11 @@ module.exports = function(grunt) {
       src_js: ['./src/**/*.js'],
       gruntfiles: ['./gruntfile.js', 'grunt.conf.js']
     },
-    /////////////////
+    /*
+     * Karma is used to run unit tests.
+     * It can run continiously (similar to the watch task).
+     *
+     */
     karma: {
       options: {
         configFile: '<%= build_dir %>/config/karma.conf.js'
@@ -281,16 +345,27 @@ module.exports = function(grunt) {
         autoWatch: true
       }
     },
-    /////////////////
+    /*
+     * Protractor is used to run E2E tests. (need a running
+     * selenium server).
+     *
+     * To run locally, 'grunt test', 'grunt test:e2e' and
+     * 'grunt protractor:build' will do the job.
+     *
+     * It also contains configuration for Travis CI.
+     * Both '/build' (development) and '/application' (
+     * production) paths are covered.
+     *
+     */
     protractor: {
       options: {
         keepAlive: true, // If false, the grunt process stops when the test fails.
-        noColor: false, // If true, protractor will not use colors in its output.
+        noColor: false,
         args: {}
       },
       build: {
         options: {
-          configFile: './config/protractor.conf.js', // Default config file
+          configFile: './config/protractor.conf.js', //Local tests
         }
       },
       ci_dev: {
@@ -304,7 +379,12 @@ module.exports = function(grunt) {
         }
       }
     },
-    /////////////////
+    /*
+     * The `watch` task builds JavaScript, HTML and all assets
+     * (including tests and the gruntfiles themselves) when any
+     * of them are changed.
+     *
+     */
     watch: {
       options: {
         livereload: true
@@ -343,15 +423,85 @@ module.exports = function(grunt) {
     }
   };
 
-  /////////////////
+  /*
+   * When initializing Grunt, it needs to be extended with the
+   * 'gruntConfig' object (imported from './config/grunt.conf.js')
+   *
+   */
   grunt.initConfig(
     grunt.util._.extend(gruntTasks, gruntConfig)
   );
 
   /*
-   * Helpers
+   * Tasks.
+   * Running 'grunt' should be enough for development.
+   * When ready for production, 'grunt compile' will compile the
+   * application into the './application' directory.
+   *
    */
-   /////////////////
+  grunt.registerTask('default', ['build', 'karma:unit', 'watch']);
+  grunt.registerTask('build', [
+    'jshint',
+    'copy:build_app_js',
+    'copy:build_app_data',
+    'copy:build_vendor_js',
+    'copy:build_views',
+    'copy:build_shared_views',
+    'copy:build_fonts',
+    'copy:build_index',
+    'copy:build_unit',
+    'copy:build_protractor',
+    'copy:build_karma',
+    'copy:build_assets',
+    'less:build_less',
+    'cssmin:build_css',
+    'clean:build_css_clean'
+  ]);
+  grunt.registerTask('compile', [
+    'jshint',
+    'clean:build_clean',
+    'build',
+    'copy:compile_views',
+    'html2js:compile_shared_views',
+    'uglify:compile_module_js',
+    'concat:compile_js',
+    'uglify:compile_soft_uglify',
+    'clean:compile_js_clean',
+    'clean:compile_shared_views_clean',
+    'copy:compile_app_data',
+    'copy:compile_app_assets',
+    'copy:compile_index'
+  ]);
+  grunt.registerTask('test', ['karma:unit', 'protractor:build']);
+  grunt.registerTask('test:unit', ['karma:unit']);
+  grunt.registerTask('test:ci', ['karma:unit', 'protractor:ci_dev', 'protractor:ci_prod']);
+  grunt.registerTask('test:e2e', ['protractor:build']);
+  grunt.registerTask('dev', ['shell:dev_server']);
+  grunt.registerTask('prod', ['shell:prod_server']);
+
+  /*
+   * Helper methods.
+   * The methods below use the 'module_file_order' Array in
+   * 'grunt.conf.js' to easily load modules in a consistent
+   * manner, generate globbing patterns or process files.
+   *
+   * (i.e. the 'index.html' is processed in order to
+   * automatically write <script> tags)
+   *
+   */
+
+
+  /*
+   * Generates a globbing pattern that matches the
+   * 'module_file_order' Array in 'grunt.conf.js'.
+   *
+   * @param {String} modulePath - The module path
+   * @returns {Array} output - An Array of globbing patterns
+   *
+   * (i.e. findModuleFilesIn('./src/app/') loads all module
+   * files in './src/app' in the defined order)
+   *
+   */
   function findModuleFilesIn(modulePath){
     var output = [],
         i;
@@ -363,7 +513,17 @@ module.exports = function(grunt) {
     return output;
   }
 
-  //////////////
+  /*
+   * 'Manually globs (using 'node-glob')' for all JavaScript files
+   * that should be included in the build phase, then stores them
+   * into the 'buildScripts' Array. 'buildScripts' is then passed * as 'scripts' to the template.
+   * The template is processed (usually loops through 'scripts').
+   * (See 'index.html')
+   *
+   * @param {String} content - File content
+   * @returns {String} - Processed content
+   *
+   */
   function processBuildScripts(content){
     var buildScripts = [],
         globFiles,
@@ -385,45 +545,4 @@ module.exports = function(grunt) {
       }
     });
   }
-
-  /*
-   * Tasks
-   */
-  grunt.registerTask('default', ['jshint', 'build', 'karma:unit', 'watch']);
-  grunt.registerTask('build', [
-    'copy:build_app_js',
-    'copy:build_app_data',
-    'copy:build_vendor_js',
-    'copy:build_views',
-    'copy:build_shared_views',
-    'copy:build_fonts',
-    'copy:build_index',
-    'copy:build_unit',
-    'copy:build_protractor',
-    'copy:build_karma',
-    'copy:build_assets',
-    'less:build_less',
-    'cssmin:build_css',
-    'clean:build_css_clean'
-  ]);
-  grunt.registerTask('compile', [
-    'clean:build_clean',
-    'build',
-    'copy:compile_views',
-    'html2js:compile_shared_views',
-    'uglify:compile_module_js',
-    'concat:compile_js',
-    'uglify:compile_cleanup',
-    'clean:compile_js_clean',
-    'clean:compile_shared_views_clean',
-    'copy:compile_app_data',
-    'copy:compile_app_assets',
-    'copy:compile_index'
-  ]);
-  grunt.registerTask('test', ['karma:unit', 'protractor:build']);
-  grunt.registerTask('test:unit', ['karma:unit']);
-  grunt.registerTask('test:ci', ['karma:unit', 'protractor:ci_dev', 'protractor:ci_prod']);
-  grunt.registerTask('test:e2e', ['protractor:build']);
-  grunt.registerTask('dev', ['shell:dev_server']);
-  grunt.registerTask('prod', ['shell:prod_server']);
 };
